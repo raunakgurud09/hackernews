0e96137ead15366f607ee210eeb1a03b7eb2ac2d
raunakgurud09 <raunakgurud2121@gmail.com>
2024-12-21 20:35:29 +0530

    refactore: moved variable for api endpoint

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,5 @@
 export const AVATAR_URL = "https://avatar.iran.liara.run/public/";
+
 export const BASE_HN_URL = "https://news.ycombinator.com";
 
 export const HN_USER_INFO_PAGE = "https://news.ycombinator.com/user?id=";
@@ -6,11 +7,11 @@ export const HN_USER_INFO_PAGE = "https://news.ycombinator.com/user?id=";
 export const API_BASE_URL = "https://hacker-news.firebaseio.com/v0";
 
 export const apiEndpoints: Record<string, string> = {
-  top: "https://hacker-news.firebaseio.com/v0/topstories.json",
-  new: "https://hacker-news.firebaseio.com/v0/newstories.json",
-  best: "https://hacker-news.firebaseio.com/v0/beststories.json",
-  job: "https://hacker-news.firebaseio.com/v0/jobstories.json",
-  show: "https://hacker-news.firebaseio.com/v0/showstories.json",
-  ask: "https://hacker-news.firebaseio.com/v0/askstories.json",
-  comment: "https://hacker-news.firebaseio.com/v0/item/",
+  top: `${API_BASE_URL}/topstories.json`,
+  new: `${API_BASE_URL}/newstories.json`,
+  best: `${API_BASE_URL}/beststories.json`,
+  job: `${API_BASE_URL}/jobstories.json`,
+  show: `${API_BASE_URL}/showstories.json`,
+  ask: `${API_BASE_URL}/askstories.json`,
+  comment: `${API_BASE_URL}/item/`,
 };
